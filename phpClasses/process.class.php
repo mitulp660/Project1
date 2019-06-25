913a1b843d2ddfecb2a6354326dd9416ca944430
mitul <mrp68@njit.edu>
2019-06-25 19:28:46 -0400

    constructed to check the value from CSV file

---
+++ phpClasses/process.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class create{
+
+  public $Name;
+  public $School;
+  public $GraduateDate;
+  public $Major;
+  public $Degree;
+
+  public function __construct($values=[]){
+  //?? is coalescing operator is going to check the value$this-> School= $values['School'] ?? '';
+    $this-> Name= $values['Name'] ?? '';
+    $this-> School= $values['School'] ?? '';
+    $this->GraduateDate = $values['GraduateDate'] ?? '';
+    $this->Major = $values['Major'] ?? '';
+    $this->Degree = $values['Degree'] ?? '';
+
+
+  }
+
+
+
+}
+
+
+
+
+
+?>
--- read.md
+++ read.md
@@ -3,3 +3,9 @@
 1.Creat a file and upload it to github
 
 2.get ready to creat codes to read file and use of OOP
+       
+       1. Created CSV file 
+       2. Creating a file class
+       3. Creating process class
+       4. index which will hold the CSV file 
+       
